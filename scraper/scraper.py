ac659c6236fd8d14fa42a30e315a102a7156723f
Jamie Kirkwin <jkirkwin@gmail.com>
2020-05-25 11:36:39 -0700

    Add initial files for selenium scraper

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,2 +1,25 @@
-'''Template to be replaced by the web scraper'''
-print("Hello")
+'''Entry point script for the web scraper.
+
+Uses Selenium to scrape the UVic academic calendar for course relationships.
+'''
+import chromedriver_autoinstaller
+import selenium.webdriver
+
+
+URL = r'https://www.uvic.ca/calendar/future/undergrad/index.php#/courses'
+
+
+def getWebdriver(headless=True):
+    chromedriver_autoinstaller.install()
+    opts = selenium.webdriver.ChromeOptions()
+    driver = selenium.webdriver.Chrome(options=opts)
+    return driver
+
+
+if __name__ == '__main__':      
+    try: 
+        driver = getWebdriver()
+        driver.get(URL)
+        driver.close()
+    except Exception as e:
+        print(repr(e))
