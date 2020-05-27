3c14c8e276292cf4b9be74f7ff5043091872592e
Jonathan Mah <jonlockm.11@gmail.com>
2020-05-26 19:25:56 -0700

    configured database

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 /*
  * This file is the entry point for the Node app.
  */
-
-const express = require('express');
+const express = require('express'); 
 const app = express();
 
 // use the PORT environment varaible if it is defined.
@@ -15,3 +14,28 @@ function onListen() {
     console.log(`Listening on port ${port}`);
 }
 app.listen(port, onListen);
+
+// connect to heroku database
+const { Client } = require('pg');
+
+const client = new Client({
+    host: 'ec2-18-233-32-61.compute-1.amazonaws.com',
+    port: 5432,
+    user: 'gadyrjddeeskwc',
+    password: '8a6d028082f3a13122125822711519bb0b547f597301b1e6006f89978c04e5aa',
+    database: 'd4i5njekim7upa',
+    ssl: {
+        rejectUnauthorized: false
+      }
+  })
+
+client.connect();
+
+// print test_table
+client.query('SELECT * FROM test_table;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
