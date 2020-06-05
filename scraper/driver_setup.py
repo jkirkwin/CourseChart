cb8b7c3f2f7577158066dc6e93a47d33386edaef
Jamie Kirkwin <jkirkwin@gmail.com>
2020-06-04 18:17:35 -0700

    Allow scraper to run chrome/driver using path

--- scraper/driver_setup.py
+++ scraper/driver_setup.py
@@ -1,7 +1,7 @@
 '''Provides functions to create a WebDriver using the current environment.
 
 This module relies on the presence of environment variables which point to the
-binaries for Google Chrome and the Chrome web driver.
+binaries for Google Chrome and the Chrome web driver in order to build on Heroku.
 
 The Chrome "buildpack" for heroku defines the location of the Chrome binary as
 'GOOGLE_CHROME_SHIM'.
@@ -10,9 +10,10 @@ See https://github.com/heroku/heroku-buildpack-google-chrome.
 The path to the Chrome web driver binary must be stored in environment variable
 CHROMEDRIVER_PATH. This has been set as a "Config Var" on Heroku.
 
-For local development, you will need to install both Chrome and the ChromeDriver
-(https://chromedriver.chromium.org/downloads) and set the above environment
-variables to the correct locations.
+For local development, you will need to install both Chrome and the 
+ChromeDriver (https://chromedriver.chromium.org/downloads) and either set the
+above environment variables to the correct locations or add the Chrome and 
+Chromedriver binaries to your PATH.
 '''
 
 import logging
@@ -35,12 +36,17 @@ def _get_chromedriver(headless):
     '''Creates and returns a ChromeDriver'''
     opts = selenium.webdriver.ChromeOptions()
     opts.headless = headless
+    
     chrome_bin = _try_get_chome_binary_path()
-    opts.binary_location = chrome_bin
+    if chrome_bin:
+        opts.binary_location = chrome_bin
 
     chromedriver_path = _try_get_chromedriver_path()
-    chrome_driver = selenium.webdriver.Chrome(chromedriver_path, options=opts)
-
+    if chromedriver_path:
+        chrome_driver = selenium.webdriver.Chrome(chromedriver_path, options=opts)
+    else:
+        chrome_driver = selenium.webdriver.Chrome(options=opts)
+    
     # Allow for a small amount of latency for each action
     chrome_driver.implicitly_wait(1)
 
@@ -50,26 +56,24 @@ def _get_chromedriver(headless):
 
 def _try_get_chromedriver_path():
     '''Returns the path to the executable for the Chrome WebDriver using the
-    environment or raises a KeyError.
-    '''
+    environment or None'''
     return _try_get_env_var(CHROMEDRIVER_BINARY)
-
 
 
 def _try_get_chome_binary_path():
     '''Returns the path to the executable for Chrome using the environment or
-    raises a KeyError.'''
+    None'''
     return _try_get_env_var(GOOGLE_CHROME_BINARY)
 
 
 def _try_get_env_var(name):
     '''Tries to read the given variable from the environment
 
-    If no such variable is defined, a KeyError is raised and an error message
-    is logged.
+    If no such variable is defined, a message is logged and the function 
+    returns None.
     '''
     try:
         return environ[name]
     except KeyError:
-        LOGGER.error("No '%s' environment variable set", name)
-        raise
+        LOGGER.info("No '%s' environment variable set", name)
+        return None
